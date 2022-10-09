f2c328a13e340f7bf20be62daac75b913e57204d
Vitor Neves <vitor.sneves@hotmail.com>
2022-10-09 17:11:37 -0300

    Fix sales_opportunity swing if no cars

--- app/app.py
+++ app/app.py
@@ -27,6 +27,11 @@ def index():
             car_citizen = db.session.query(Citizen.name, db.func.count(Car.citizen_id)) \
                 .outerjoin(Car, Citizen.id == Car.citizen_id).group_by(Citizen.name).all()
             car_citizen = dict(car_citizen)
+
+            # for k, v in car_citizen.items():
+            #     if v == 0:
+
+
             return render_template('index.html', citizens=citizens, user=user,
                                     car_citizen=car_citizen)
     else:
@@ -49,8 +54,8 @@ def logout():
     session.pop('user', None)
     return redirect(url_for('login'))
 
-@app.route('/delete/<int:id>')
-def delete(id):
+@app.route('/delete_car/<int:id>')
+def delete_car(id):
     row_to_delete = Car.query.get(id)
     citizen_id = row_to_delete.citizen_id
 
@@ -62,6 +67,18 @@ def delete(id):
     except:
         return 'Error deleting'
 
+@app.route('/delete_citizen/<int:id>')
+def delete_citizen(id):
+    row_to_delete = Citizen.query.get(id)
+    print(row_to_delete.name)
+    try:
+        db.session.delete(row_to_delete)
+        db.session.commit()
+
+        return redirect('/')
+    except:
+        return 'Error deleting'
+
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     person = Citizen.query.get_or_404(id)
@@ -69,6 +86,9 @@ def update(id):
         .outerjoin(Car, Citizen.id == Car.citizen_id).order_by(Car.id).all()
 
     list_car_citizen = [tup for tup in car_citizen if tup[0] == person.name]
+    if list_car_citizen[0][1] == None:
+        person.sale_opportunity = True
+        db.session.commit()
 
     if request.method == 'POST':
         if len(list_car_citizen) < 3:
